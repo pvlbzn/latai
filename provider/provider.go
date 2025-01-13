8892ee6e2cc162a376a78789b7b1333f328e1ae4
Pavel Bazin <pvlbzn@gmail.com>
2025-01-13 19:36:34 -0300

    feat: provider contract

--- provider/provider.go
+++ provider/provider.go
@@ -1,17 +1,46 @@
 package provider
 
-type Prompt struct {
-	Description string
-	Content     string
-}
+import (
+	"time"
 
-type Metrics struct {
-	Provider string
-	Model    string
-	Latency  string
-	Tokens   string
-}
+	"github.com/pvlbzn/genlat/prompt"
+)
 
+// Provider is a core interface for each provider implementation
+// to satisfy. Each service, such as AWS Bedrock or OpenAI, should
+// provide implementation of Provider.
 type Provider interface {
-	Run() error
+	// GetModels returns list of available models at provider.
+	GetModels(filter string) ([]*Model, error)
+
+	// Measure measures a particular model and returns Metric back.
+	Measure(model *Model, prompt *prompt.Prompt) (*Metric, error)
+
+	// Send a message to LLM. Can be used stand alone, and is used
+	// by Measure internally to make calls to gather metrics.
+	Send(message string, to *Model) (string, error)
+}
+
+// Model holds key characteristics of a particular model instance.
+type Model struct {
+	// Model identification, oftentimes model's full name.
+	ID string
+
+	// Model human-readable name.
+	Name string
+
+	// Model service provider, that is name of a service model
+	// is being served from such as AWS, OpenAI, Grok, etc.
+	Provider string
+
+	// Model vendor, that is name of a company which built
+	// the model itself such as Anthropic, Google, Amazon, etc.
+	Vendor string
+}
+
+// Metric wraps model data and provides Latency extra field.
+type Metric struct {
+	Model    *Model
+	Latency  time.Duration
+	Response string
 }
