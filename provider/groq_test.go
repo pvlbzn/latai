4d35c64d0e9f401b7db6e0fdda94b68f671bb9bf
Pavel Bazin <pvlbzn@gmail.com>
2025-02-05 00:27:27 -0300

    test: groq and openai tests

--- provider/groq_test.go
+++ provider/groq_test.go
@@ -3,33 +3,48 @@ package provider
 import "testing"
 
 func TestGroqSendGemmaFamily(t *testing.T) {
-	groqSendHelper(t, "Gemma 2 9B IT")
-}
-
-func TestGroqSendLlama3Family(t *testing.T) {
-	groqSendHelper(t, "Llama 3.3 70b Versatile")
-	groqSendHelper(t, "Llama 3.1 8b Instant")
-	groqSendHelper(t, "Llama Guard 3 8B")
-	groqSendHelper(t, "Llama3 70b 8192")
-	groqSendHelper(t, "Llama3 8b 8192")
-	groqSendHelper(t, "Llama 3.2 1b Preview")
-	groqSendHelper(t, "Llama 3.2 3b Preview")
-}
-
-func TestGroqSendMixtralFamily(t *testing.T) {
-	groqSendHelper(t, "Mixtral 8x7b 32768")
-}
-
-func TestGroqSendR1Family(t *testing.T) {
-	groqSendHelper(t, "DeepSeek R1 Distill Llama 70B")
-}
-
-func groqSendHelper(t *testing.T, modelName string) {
 	c, err := NewGroq("")
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	sendHelper(t, c, "Gemma 2 9B IT")
+}
+
+func TestGroqSendLlama3Family(t *testing.T) {
+	c, err := NewGroq("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sendHelper(t, c, "Llama 3.3 70b Versatile")
+	sendHelper(t, c, "Llama 3.1 8b Instant")
+	sendHelper(t, c, "Llama Guard 3 8B")
+	sendHelper(t, c, "Llama3 70b 8192")
+	sendHelper(t, c, "Llama3 8b 8192")
+	sendHelper(t, c, "Llama 3.2 1b Preview")
+	sendHelper(t, c, "Llama 3.2 3b Preview")
+}
+
+func TestGroqSendMixtralFamily(t *testing.T) {
+	c, err := NewGroq("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sendHelper(t, c, "Mixtral 8x7b 32768")
+}
+
+func TestGroqSendR1Family(t *testing.T) {
+	c, err := NewGroq("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sendHelper(t, c, "DeepSeek R1 Distill Llama 70B")
+}
+
+func sendHelper(t *testing.T, c Provider, modelName string) {
 	m := c.GetLLMModels(modelName)
 	if len(m) == 0 {
 		t.Fatal(modelName + " should not be empty")
--- provider/openai_test.go
+++ provider/openai_test.go
@@ -1,119 +1,41 @@
 package provider
 
-import (
-	"strings"
-	"testing"
-)
+import "testing"
 
-func TestOpenAIid2Name(t *testing.T) {
-	tests := []struct {
-		in       string
-		expected string
-	}{
-		{
-			in:       "gpt-4o-realtime-preview",
-			expected: "Gpt 4o Realtime Preview",
-		},
-		{
-			in:       "o1-mini",
-			expected: "O1 Mini",
-		},
-	}
-
-	client, err := NewOpenAI("")
+func TestOpenAISendGPTFamily(t *testing.T) {
+	c, err := NewOpenAI("")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	for _, tt := range tests {
-		res := client.id2Name(tt.in)
-		if res != tt.expected {
-			t.Errorf("id2Name(%s) got %s, want %s", tt.in, res, tt.expected)
-		}
-	}
-}
+	sendHelper(t, c, "GPT 4 1106 Preview")
+	sendHelper(t, c, "GPT 3.5 Turbo")
+	sendHelper(t, c, "GPT 3.5 Turbo 0125")
+	sendHelper(t, c, "GPT 3.5 Turbo 16k")
+	sendHelper(t, c, "GPT 4")
+	sendHelper(t, c, "GPT 4 0613")
+	sendHelper(t, c, "ChatGPT 4o Latest")
+	sendHelper(t, c, "GPT 4o 2024 08 06")
+	sendHelper(t, c, "GPT 4o")
+	sendHelper(t, c, "GPT 3.5 Turbo 1106")
+	sendHelper(t, c, "GPT 4 Turbo 2024 04 09")
+	sendHelper(t, c, "GPT 4 Turbo")
+	sendHelper(t, c, "GPT 4 Turbo Preview")
+	sendHelper(t, c, "GPT 4o 2024 05 13")
+	sendHelper(t, c, "GPT 4o 2024 11 20")
+	sendHelper(t, c, "GPT 4o Mini 2024 07 18")
+	sendHelper(t, c, "GPT 4o Mini")
+	sendHelper(t, c, "GPT 4 0125 Preview")
 
-// TestGetModels executes `GetModels` and verifies that models returned
-// correctly. It verifies just a few models out of the list because
-// the list gets updates with some frequency.
-func TestOpenAIGetModels(t *testing.T) {
-	expectedModels := []string{
-		"gpt-4o-realtime-preview-2024-12-17",
-		"gpt-4o-mini-audio-preview",
-		"dall-e-3",
-		"o1-preview",
-		"gpt-4-turbo-2024-04-09",
-	}
-
-	client, err := NewOpenAI("")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	models, err := client.GetAllModels("")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Build a lookup table so all the future checks will be
-	// O(1) instead of O(n)
-	lookup := make(map[string]string)
-	for _, m := range models {
-		lookup[m.ID] = m.Name
-	}
-
-	for _, m := range expectedModels {
-		_, found := lookup[m]
-		if !found {
-			t.Errorf("model `%s` not found", m)
-		}
-	}
-}
-
-func TestOpenAIGetLLMModelsSearch(t *testing.T) {
-	client, err := NewOpenAI("")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	models, err := client.GetLLMModels("o1 mini")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(models) != 2 {
-		t.Errorf("got %d models, expected 2", len(models))
-	}
-
-	if models[0].ID != "o1-mini" {
-		t.Errorf("got %s, expected o1-mini", models[0].ID)
-	}
-
-	if models[1].ID != "o1-mini-2024-09-12" {
-		t.Errorf("got %s, expected o1-mini-2024-09-12", models[1].ID)
-	}
-}
-
-func TestOpenAISend(t *testing.T) {
-	client, err := NewOpenAI("")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	models, err := client.GetLLMModels("o1 mini")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	prompt := "Reply a single word 'Assistant'"
-	expected := "Assistant"
-
-	res, err := client.Send(prompt, models[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if strings.TrimSpace(res) != expected {
-		t.Errorf("got %s, want %s", res, expected)
-	}
+	// These tests are disabled due to their price, and likelihood
+	// of being correct if all the above runs successfully since
+	// API is identical.
+	/*
+		sendHelper(t, c, "O1")
+		sendHelper(t, c, "O1 Preview 2024 09 12")
+		sendHelper(t, c, "O1 Preview")
+		sendHelper(t, c, "O1 Mini")
+		sendHelper(t, c, "O1 Mini 2024 09 12")
+		sendHelper(t, c, "O1 2024 12 17")
+	*/
 }
